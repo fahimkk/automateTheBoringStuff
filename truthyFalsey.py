545fe1b8a7c318b3c585ca80b7186e9f116c8e5e
fahimkk <fahimasharafkk@gmail.com>
2020-08-01 23:58:59 +0530

    added if part to the truthyFalsey file

--- truthyFalsey.py
+++ truthyFalsey.py
@@ -3,3 +3,7 @@ while not name:
     print('Enter your Name:')
     name = input()
 print('How many guest will you have?')
+noOfGust=int(input())
+if noOfGust :
+    print('Be sure to have enough room for all your guest')
+print('Done')
