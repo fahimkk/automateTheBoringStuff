ef9c2332a1f2002c296c98183f8b74efda788b4d
fahimkk <fahimasharafkk@gmail.com>
2020-08-15 15:56:26 +0530

    chapter_14 Google SpreadSheet

---
+++ chapter_14/test.py
@@ -0,0 +1,7 @@
+#ss = ezsheets.Spreadsheet('1LRMzCmcT-wJl0jXCpzWEAE23tfPlmX3Db1geZOBWdt0/edit#gid=0')
+# 1004853098672-3hs2082k2rllvsncfqvqsiilgn7nundc.apps.googleusercontent.com
+# eRnKW0yx03xOyXEPOnQp9axc
+import ezsheets
+
+ss = ezsheets.Spreadsheet('1jDZEdvSIh4TmZxccyy0ZXrH-ELlrwq8_YYiZrEOB4jg')
+ss.downloadAsExcel()
